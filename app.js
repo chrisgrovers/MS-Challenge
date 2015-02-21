e05771bd2d9c6db9de44ccbecf0cfe52a3ddb17f
Chris Grovers <chrisgrovers@users.noreply.github.com>
2015-02-20 23:42:52 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
 
-	function decreaseVal($changeNumber, by){
-			return parseInt($changeNumber)-by;
+	function decreaseVal(changeNumber, by){
+			var answer = changeNumber-by;
+			inventory.ingredients['sugar'] = answer;
+			console.log(answer);
+			
 	}
+
 	function increaseVal($changeNumber, by){
 			return parseInt($changeNumber)+by;
 	}
@@ -25,28 +29,42 @@ $(document).ready(function() {
 
 	//click on use sugar
 	$(".use-sugar").click(function(){
-		$('#ingredients .sugar').text(decreaseVal(inventory.ingredients.sugar, 1)),
-		$('#pot .sugar').text(increaseVal(inventory.pot.sugar, 1));
+		//function is only being carried out once. Need to trigger function multiple times
+		decreaseVal(inventory.ingredients.sugar, 1);
+		
+		$('#ingredients .sugar').text(inventory.ingredients.sugar);
+		// increaseVal(inventory.pot.sugar, 1)
+
+		// $('#pot .sugar').text(inventory.pot.sugar);
 	});
 	//click on use flour
 	$('.use-flour').click(function(){
-		$('#ingredients .flour').text(decreaseVal(inventory.ingredients.flour, 1)),
-		$('#pot .flour').text(increaseVal(inventory.pot.flour, 1));
+		decreaseVal(inventory.ingredients.flour, 1)
+		$('#ingredients .flour').text(inventory.ingredients.flour),
+		increaseVal(inventory.pot.flour, 1)
+		$('#pot .flour').text(inventory.pot.flour);
 	});
 	//click on make cookie
 	$('#pot button').click(function(){
-		$('#pot .flour').text(decreaseVal(inventory.pot.flour, 6)),
-		$('#pot .sugar').text(decreaseVal(inventory.pot.sugar, 3)),
-		$('#product .cookies').text(increaseVal(inventory.product.cookies, 1));
+		decreaseVal(inventory.pot.flour, 6)
+		$('#pot .flour').text(inventory.pot.flour),
+		decreaseVal(inventory.pot.sugar, 3)
+		$('#pot .sugar').text(inventory.pot.sugar),
+		increaseVal(inventory.product.cookies, 1)
+		$('#product .cookies').text(inventory.product.cookies);
 	});
 	// buy sugar for $10
 	$('.buy-sugar').click(function(){
-		$('#ingredients .sugar').text(decreaseVal(inventory.ingredients.sugar, 1)),
-		$('#product .money').text(decreaseVal(inventory.product.money, 10));
+		decreaseVal(inventory.ingredients.sugar, 1)
+		$('#ingredients .sugar').text(inventory.ingredients.sugar),
+		decreaseVal(inventory.product.money, 10)
+		$('#product .money').text(inventory.product.money);
 	})
 	// buy flour for $15
 	$('.buy-flour').click(function(){
-		$('#ingredients .flour').text(increaseVal(inventory.ingredients.flour, 1)),
-		$('#product .money').text(decreaseVal(inventory.product.money, 15));
+		increaseVal(inventory.ingredients.flour, 1)
+		$('#ingredients .flour').text(inventory.ingredients.flour),
+		decreaseVal(inventory.product.money, 15)
+		$('#product .money').text(inventory.product.money);
 	})
 });
