37288c5faae8f4b502c119c9f705b7ad0ec7b5aa
Chris Grovers <chrisgrovers@users.noreply.github.com>
2015-02-26 12:20:16 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,57 +1,5 @@
 $(document).ready(function() {
 
-	function decreaseVal(changeNumber, by){
-			var answer = changeNumber-by;
-			inventory.ingredients['sugar'] = answer;
-			console.log(inventory.ingredients['sugar']);
-			
-	}
-
-	function increaseVal($changeNumber, by){
-			return parseInt($changeNumber)+by;
-	}
-	function clickSugar(){
-		if (inventory.ingredients.sugar>0){
-			var answer = inventory.ingredients.sugar-1;
-			inventory.ingredients['sugar']=answer;
-			var solution = inventory.pot.sugar+1;
-			inventory.pot['sugar']=solution;
-		}
-	}
-	function clickFlour(){
-		if(inventory.ingredients.flour>0){
-			var answer = inventory.ingredients.flour-1;
-			inventory.ingredients['flour']=answer;
-			var solution = inventory.pot.flour+1;
-			inventory.pot['flour']=solution;
-		}
-	}
-	function clickCookie(){
-		if (inventory.pot.flour>=6 && inventory.pot.sugar>=3){
-			var answer = inventory.pot.flour-6;
-			inventory.pot['flour']=answer;
-			var solution = inventory.pot.sugar-3;
-			inventory.pot['sugar']=solution;
-			var revolution = inventory.product.cookies+1;
-			inventory.product['cookies']=revolution;
-		}
-	}
-	function buyFlour(){
-		if (inventory.product.money >=15)
-			var answer=inventory.product.money-15;
-			inventory.product['money']=answer;
-			var solution=inventory.ingredients.flour+1;
-			inventory.ingredients['flour']=solution;
-	}
-	function buySugar(){
-		if (inventory.product.money >=15){
-			var answer=inventory.product.money-10;
-			inventory.product['money']=answer;
-			var solution=inventory.ingredients.sugar+1;
-			inventory.ingredients['sugar']=solution;
-		}
-	}
-
 	var inventory ={
 		product: {
 			money: 1000,
@@ -66,51 +14,74 @@ $(document).ready(function() {
 			flour: 0
 		}
 	};
-	//TODO Push the inventory values to the html page
+
+	// function to update html
+	function updateHTML(){
+		$('#product .cookies').text(inventory.product.cookies);
+		$('#product .money').text(inventory.product.money);
+		$('#ingredients .sugar').text(inventory.ingredients.sugar);
+		$('#ingredients .flour').text(inventory.ingredients.flour);
+		$('#pot .sugar').text(inventory.pot.sugar);
+		$('#pot .flour').text(inventory.pot.flour);
+	}
+
+	// TODO: function to modify object
+	function updateInventory(inventory, name, amt){
+		inventory[name]+=amt;
+		updateHTML();
+	}
 
 	//click on use sugar
 	$(".use-sugar").click(function(){
-		//function is only being carried out once. Need to trigger function multiple times
-		clickSugar();
-		//decreaseVal(inventory.ingredients.sugar, 1);
-		
-		$('#ingredients .sugar').text(inventory.ingredients.sugar);
-		// increaseVal(inventory.pot.sugar, 1)
-
-		$('#pot .sugar').text(inventory.pot.sugar);
+		if (inventory.ingredients.sugar>0){
+			updateInventory(inventory.ingredients, 'sugar', -1);
+			updateInventory(inventory.pot, 'sugar', 1);
+		}
 	});
 	//click on use flour
 	$('.use-flour').click(function(){
-		clickFlour();
-		// decreaseVal(inventory.ingredients.flour, 1)
-		$('#ingredients .flour').text(inventory.ingredients.flour),
-		// increaseVal(inventory.pot.flour, 1)
-		$('#pot .flour').text(inventory.pot.flour);
+		if(inventory.ingredients.flour>0){
+			updateInventory(inventory.ingredients, 'flour', -1);
+			updateInventory(inventory.pot, 'flour', 1);
+		}
 	});
 	//click on make cookie
 	$('#pot button').click(function(){
-		clickCookie();
-		// decreaseVal(inventory.pot.flour, 6)
-		$('#pot .flour').text(inventory.pot.flour),
-		// decreaseVal(inventory.pot.sugar, 3)
-		$('#pot .sugar').text(inventory.pot.sugar),
-		// increaseVal(inventory.product.cookies, 1)
-		$('#product .cookies').text(inventory.product.cookies);
+		if (inventory.pot.flour>=6 && inventory.pot.sugar>=3){
+			updateInventory(inventory.pot, 'flour', -6);
+			updateInventory(inventory.pot, 'sugar', -3);
+			updateInventory(inventory.product, 'cookies', 1);
+		}
 	});
 	// buy sugar for $10
 	$('.buy-sugar').click(function(){
-		buySugar();
-		// decreaseVal(inventory.ingredients.sugar, 1)
-		$('#ingredients .sugar').text(inventory.ingredients.sugar),
-		// decreaseVal(inventory.product.money, 10)
-		$('#product .money').text(inventory.product.money);
+		if (inventory.product.money >=15){
+			updateInventory(inventory.product, 'money', -10);
+			updateInventory(inventory.ingredients, 'sugar', 1);
+		}
 	})
 	// buy flour for $15
 	$('.buy-flour').click(function(){
-		buyFlour();
-		// increaseVal(inventory.ingredients.flour, 1)
-		$('#ingredients .flour').text(inventory.ingredients.flour),
-		// decreaseVal(inventory.product.money, 15)
-		$('#product .money').text(inventory.product.money);
+		if (inventory.product.money >=15) {
+			updateInventory(inventory.product, 'money', -15);
+			updateInventory(inventory.ingredients, 'flour', 1);
+		}
 	})
+
+	var findSmallestDifference = function(arr) {
+	  // Your code goes here
+	  arr.sort(function(a,b){return a-b});
+	  var smallest = 'Please enter more numbers'
+	  for (var i=1;i<arr.length;i++){
+	  	if (i==1)
+	  		smallest=arr[i]-arr[i-1];
+	  	else if(smallest>(arr[i]-arr[i-1]))
+	  		smallest=(arr[i]-arr[i-1]);
+	  }
+	  return smallest;
+	};
+
+	var result = findSmallestDifference([100, 500, 300, 1000, -200, 990]);
+	console.log(result); 
+	// The answer is 10 for this example because the difference between 1000 and 990 is 10
 });
